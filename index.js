9114ef75fe20d6d47870bb7951359e194d1d9400
Satyajeet Pal <pal@Satyajeets-MacBook-Pro.local>
2017-12-21 22:11:31 -0500

    Print out port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const
     bodyParser = require('body-parser'),
     app = express().use(bodyParser.json());
 
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening on port ${process.env.PORT}'));
+app.listen(process.env.PORT || 1337, () => console.log(`webhook is listening on port ${process.env.PORT}`));
 
 app.post('/webhook', (req, res) => {
 
