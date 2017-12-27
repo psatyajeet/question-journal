d5051712733151f6877a24732704f51ab8dd9872
Satyajeet Pal <pal@Satyajeets-MacBook-Pro.local>
2017-12-27 12:34:05 -0500

    Uncomment call to facebook graph API

--- index.js
+++ index.js
@@ -139,18 +139,18 @@ function callSendAPI(sender_psid, response) {
     }
 
     // Send the HTTP request to the Messenger Platform
-    // request({
-    //     "uri": "https://graph.facebook.com/v2.6/me/messages",
-    //     "qs": { "access_token": PAGE_ACCESS_TOKEN },
-    //     "method": "POST",
-    //     "json": request_body
-    // }, (err, res, body) => {
-    //     if (!err) {
-    //         console.log('message sent!');
-    //     } else {
-    //         console.error("Unable to send message:" + err);
-    //     }
-    // }); 
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!');
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    }); 
 }
 
 function listEntries(psid, month, date, response, sendFunction) {
